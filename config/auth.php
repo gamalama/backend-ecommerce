3c93960e7893f30eca50fad571cf5e96fc3dc7e9
gamalama <fakhry.mmpp@gmail.com>
2023-10-09 11:30:53 +0800

    Instalasi dan Konfigurasi JWT untuk Admin
    composer require php-open-source-saver/jwt-auth:1.4.2
    php artisan vendor:publish --provider="PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider"
    php artisan jwt:secret

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api_admin' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
+        ],
     ],
 
     /*
